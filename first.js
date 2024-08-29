9e3584fd4b42d93a6cb8b1bbaab98a132e3f3dd7
MDMonirujjaman805 <monirbinkhaled1@gmail.com>
2024-08-29 16:09:34 +0600

    frist commit

---
+++ first.js
@@ -0,0 +1,17 @@
+var fullName ="monirujjaman"          
+var age = '24';
+var price = 100;
+let heith = 5.6;
+const weathe = 60;
+
+
+
+console.log(fullName)
+console.log(age)
+
+// console.log(heith);
+// console.log(age);
+// console.log(fullName);
+// console.log(price);
+
+
